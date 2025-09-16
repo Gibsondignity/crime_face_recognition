a69cbf3ddb74655ac8b533a907e879e9eb84fbdc
Gibsondignity <gibsondignty@gmail.com>
2025-09-16 01:36:44 +0000

    update

--- facerecognititon/admin.py
+++ facerecognititon/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import *
 admin.site.register(User)
 
-admin.site.register(CriminalLastSpotted)
-admin.site.register(Criminal)
+# admin.site.register(CriminalLastSpotted)
+# admin.site.register(Criminal)
 admin.site.register(File)
 
 
